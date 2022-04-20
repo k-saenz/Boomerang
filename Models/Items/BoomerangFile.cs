50a5484def730aa5aceff354b429c8504ef44e75
ks689665 <ksaenz040@gmail.com>
2022-04-20 14:50:10 -0500

    added FormFile Implementation

--- Models/Items/BoomerangFile.cs
+++ Models/Items/BoomerangFile.cs
@@ -1,54 +1,35 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Boomerang.Models
 {
-    public class BoomerangFile : IFormFile
+    public class BoomerangFile
     {
         [Key]
         [Required]
         public int FileId { get; set; }
         public string Name { get; set; }
-        public string FileName { get; set; }
         //Stores User ID
         public string BelongsTo { get; set; }
         public DateTime? CreatedOn { get; set; }
         public byte[] Content { get; set; }
-        public string ContentType { get; set; }
-        public long Length { get; }
-
-        public string ContentDisposition => throw new NotImplementedException();
-
-        public IHeaderDictionary Headers => throw new NotImplementedException();
+        
+        public FormFile FileData { get; set; }
 
         public BoomerangFile() { }
 
-        public BoomerangFile(int fileId, string belongsTo, DateTime? createdOn, byte[] content, string fileType)
+        public BoomerangFile(int fileId, string belongsTo, DateTime? createdOn, byte[] content, FormFile data)
         {
             FileId = fileId;
             BelongsTo = belongsTo;
             CreatedOn = createdOn;
             Content = content;
-            ContentType = fileType;
-        }
-
-        public Stream OpenReadStream()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(Stream target)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task CopyToAsync(Stream target, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
+            FileData = data;
         }
     }
 
---
+++ Models/Items/FormFile.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Boomerang.Models.Items
+{
+    public class FormFile : IFormFile
+    {
+        private readonly IFormFile _file;
+
+        //Properties
+        [Key]
+        [Required]
+        public string FileDataId { get; set; }
+        public BoomerangFile File { get; set; }
+
+        public string ContentType
+        {
+            get { return _file.ContentType; }
+
+        }
+
+        [NotMapped]
+        public string ContentDisposition
+        {
+            get { return _file.ContentDisposition; }
+
+        }
+        [NotMapped]
+        public IHeaderDictionary Headers
+        {
+            get { return _file.Headers; }
+
+        }
+
+        [Column(TypeName = "BIGINT")]
+        public long Length
+        {
+            get { return _file.Length; }
+
+        }
+
+        public string Name
+        {
+            get { return _file.Name; }
+
+        }
+
+        public string FileName
+        {
+            get { return _file.FileName; }
+
+        }
+
+        //Methods
+        public void CopyTo(Stream target)
+        {
+            _file.CopyTo(target);
+        }
+
+        public Task CopyToAsync(Stream target, CancellationToken cancellationToken = default)
+        {
+            return _file.CopyToAsync(target, cancellationToken);
+        }
+
+        public Stream OpenReadStream()
+        {
+            return _file.OpenReadStream();
+        }
+    }
+}
